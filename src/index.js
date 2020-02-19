84e465a75c0d153c75043d88ceb1f9172b410227
Alejandro <orochi_alex@hotmail.com>
2020-02-19 11:33:37 -0600

    added jest test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import logo from '../public/logo.svg'
+
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = logo;
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
